68bdbff937c35742c035ae4fb106a9f18c484cbd
svladimirtsk-crypto <s.vladimir.tsk@gmail.com>
2026-03-10 14:24:27 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
 import ForWhom from "@/components/ForWhom";
+import Testimonials from "@/components/Testimonials";
 import Author from "@/components/Author";
 import Program from "@/components/Program";
+import Gallery from "@/components/Gallery";
 import Results from "@/components/Results";
 import Pricing from "@/components/Pricing";
 import CTAForm from "@/components/CTAForm";
+import FloatingCTA from "@/components/FloatingCTA";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -14,14 +17,15 @@ export default function Home() {
       <Navigation />
       <Hero />
       <ForWhom />
+      <Testimonials />
       <Author />
       <Program />
+      <Gallery />
       <Results />
       <Pricing />
       <CTAForm />
+      <FloatingCTA />
       <Footer />
     </main>
   );
-
 }
-
